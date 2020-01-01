59e0c5179a20fe9cf07e00e1ded4873ddbf80574
justinterada <jtvkws001@gmail.com>
2019-12-31 17:00:50 -0800

    Add a negative value parsing test

--- core/src/test/java/org/jcompany/grocery/parsers/ProductRecordLineParserTest.java
+++ core/src/test/java/org/jcompany/grocery/parsers/ProductRecordLineParserTest.java
@@ -36,4 +36,23 @@ public class ProductRecordLineParserTest {
         assertEquals(output.getSize(), "18oz");
         assertEquals(output.getFlags(), new boolean[] { false, false, false, false, false, false, false, false, false});
     }
+
+    @Test
+    public void parseWithNegativeValues() {
+        final String input = "80000001       Kimchi-flavored white rice                            -0123456 -0000555 -0998322 -3664738 00000100 00000200 YYYYYYYYY     1ctn ";
+
+        ProductRecordLine output = instance.parse(input);
+
+        assertNotNull(output);
+        assertEquals(output.getId(), 80000001);
+        assertEquals(output.getDescription(), "Kimchi-flavored white rice");
+        assertEquals(output.getPrice(), new BigDecimal("-1234.56"));
+        assertEquals(output.getSplitPrice(), new BigDecimal("-9983.22"));
+        assertEquals(output.getPromotionalPrice(), new BigDecimal("-5.55"));
+        assertEquals(output.getPromotionalSplitPrice(), new BigDecimal("-36647.38"));
+        assertEquals(output.getRegularFor(), 100);
+        assertEquals(output.getPromotionalFor(), 200);
+        assertEquals(output.getSize(), "1ctn");
+        assertEquals(output.getFlags(), new boolean[] { true, true, true, true, true, true, true, true, true});
+    }
 }
