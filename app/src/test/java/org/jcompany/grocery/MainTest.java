ce14e783c12dc2be0f4510b8370ed9a8bbf961f3
justinterada <jtvkws001@gmail.com>
2020-01-03 15:44:47 -0800

    Address newlines

--- app/src/test/java/org/jcompany/grocery/MainTest.java
+++ app/src/test/java/org/jcompany/grocery/MainTest.java
@@ -38,49 +38,49 @@ public class MainTest {
         assertEquals(errorOutput, "");
         String output = outContent.toString();
         assertNotNull(output);
-        assertEquals(output, "{\n" +
-                "    id: 80000001,\n" +
-                "    description: \"Kimchi-flavored white rice\",\n" +
-                "    displayPrice: \"$5.67 each\",\n" +
-                "    calculatorPrice: 5.6700,\n" +
-                "    promotionalDisplayPrice: null,\n" +
-                "    promotionalCalculatorPrice: null,\n" +
-                "    unitOfMeasure: EACH,\n" +
-                "    size: \"18oz\",\n" +
-                "    taxRate: 0.0000,\n" +
-                "}\n" +
-                "{\n" +
-                "    id: 14963801,\n" +
-                "    description: \"Generic Soda 12-pack\",\n" +
-                "    displayPrice: \"$13.00 for 2\",\n" +
-                "    calculatorPrice: 6.5000,\n" +
-                "    promotionalDisplayPrice: \"$5.49 each\",\n" +
-                "    promotionalCalculatorPrice: 5.4900,\n" +
-                "    unitOfMeasure: EACH,\n" +
-                "    size: \"12x12oz\",\n" +
-                "    taxRate: 0.0775,\n" +
-                "}\n" +
-                "{\n" +
-                "    id: 40123401,\n" +
-                "    description: \"Marlboro Cigarettes\",\n" +
-                "    displayPrice: \"$10.00 each\",\n" +
-                "    calculatorPrice: 10.0000,\n" +
-                "    promotionalDisplayPrice: \"$5.49 each\",\n" +
-                "    promotionalCalculatorPrice: 5.4900,\n" +
-                "    unitOfMeasure: EACH,\n" +
-                "    size: \"\",\n" +
-                "    taxRate: 0.0000,\n" +
-                "}\n" +
-                "{\n" +
-                "    id: 50133333,\n" +
-                "    description: \"Fuji Apples (Organic)\",\n" +
-                "    displayPrice: \"$3.49 per pound\",\n" +
-                "    calculatorPrice: 3.4900,\n" +
-                "    promotionalDisplayPrice: null,\n" +
-                "    promotionalCalculatorPrice: null,\n" +
-                "    unitOfMeasure: POUND,\n" +
-                "    size: \"lb\",\n" +
-                "    taxRate: 0.0000,\n" +
-                "}\n");
+        assertEquals(output, String.format("{%n" +
+                "    id: 80000001,%n" +
+                "    description: \"Kimchi-flavored white rice\",%n" +
+                "    displayPrice: \"$5.67 each\",%n" +
+                "    calculatorPrice: 5.6700,%n" +
+                "    promotionalDisplayPrice: null,%n" +
+                "    promotionalCalculatorPrice: null,%n" +
+                "    unitOfMeasure: EACH,%n" +
+                "    size: \"18oz\",%n" +
+                "    taxRate: 0.0000,%n" +
+                "}%n" +
+                "{%n" +
+                "    id: 14963801,%n" +
+                "    description: \"Generic Soda 12-pack\",%n" +
+                "    displayPrice: \"$13.00 for 2\",%n" +
+                "    calculatorPrice: 6.5000,%n" +
+                "    promotionalDisplayPrice: \"$5.49 each\",%n" +
+                "    promotionalCalculatorPrice: 5.4900,%n" +
+                "    unitOfMeasure: EACH,%n" +
+                "    size: \"12x12oz\",%n" +
+                "    taxRate: 0.0775,%n" +
+                "}%n" +
+                "{%n" +
+                "    id: 40123401,%n" +
+                "    description: \"Marlboro Cigarettes\",%n" +
+                "    displayPrice: \"$10.00 each\",%n" +
+                "    calculatorPrice: 10.0000,%n" +
+                "    promotionalDisplayPrice: \"$5.49 each\",%n" +
+                "    promotionalCalculatorPrice: 5.4900,%n" +
+                "    unitOfMeasure: EACH,%n" +
+                "    size: \"\",%n" +
+                "    taxRate: 0.0000,%n" +
+                "}%n" +
+                "{%n" +
+                "    id: 50133333,%n" +
+                "    description: \"Fuji Apples (Organic)\",%n" +
+                "    displayPrice: \"$3.49 per pound\",%n" +
+                "    calculatorPrice: 3.4900,%n" +
+                "    promotionalDisplayPrice: null,%n" +
+                "    promotionalCalculatorPrice: null,%n" +
+                "    unitOfMeasure: POUND,%n" +
+                "    size: \"lb\",%n" +
+                "    taxRate: 0.0000,%n" +
+                "}%n"));
     }
 }
